0d584c43498ce62d4734b80db000a31e55d92fc4
Jose Riano <joselriano10@gmail.com>
2017-09-27 05:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 function takeANumber(katzDeliLine, name){
       katzDeli.push(name);
-      console.log('Welcome, '+name+'. You are number '+ katzDeli.length +'!');
+      console.log('Welcome, '+name+'. You are number '+ katzDeli.length+1 +'!');
 
 }
